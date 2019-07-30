f863a54a61c6bec6b774d80f795e7ddfcb26298b
Diego Wagner <diegowagner4@gmail.com>
2019-07-29 23:36:25 -0300

    send constructor func in asterisk package

--- asterisk.go
+++ asterisk.go
@@ -14,3 +14,33 @@ type Asterisk struct {
 	stop   chan struct{}
 	wg     sync.WaitGroup
 }
+
+func NewAsterisk(host string, username string, secret string) (*Asterisk, error) {
+
+	socket, err := ami.NewSocket(host)
+	if err != nil {
+		return nil, err
+	}
+
+	uuid, err := ami.GetUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	const events = "system,call,all,user"
+	err = ami.Login(socket, username, secret, events, uuid)
+	if err != nil {
+		return nil, err
+	}
+
+	as := &Asterisk{
+		socket: socket,
+		uuid:   uuid,
+		events: make(chan ami.Response),
+		stop:   make(chan struct{}),
+	}
+
+	as.wg.Add(1)
+	//go as.run()
+	return as, nil
+}
